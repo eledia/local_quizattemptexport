bdecd54d93af20aac192394dc02efd9fa895cca6
Ralf Wiederhold <ralf.wiederhold@eledia.de>
2021-08-18 21:23:43 +0200

    eledia local_quizattemptexport - Improved handling of embedded images.

--- local/quizattemptexport/classes/processing/processor.php
+++ local/quizattemptexport/classes/processing/processor.php
@@ -67,42 +67,98 @@ class processor {
 
 
 
-        // Do generalized replacement of images that contain a pluginfile url.
+        // Transform any image contained within the content that is not already a data url
+        // into a data URL. This is necessary for local files served through moodle file
+        // system but will also avoid problems where wkhtmltopdf will not be able to load
+        // non-local files.
         $dom = domdocument_util::initialize_domdocument($html);
         $xpath = new \DOMXPath($dom);
         $imgs = $xpath->query('//img');
         foreach ($imgs as $img) {
 
             /** @var \DOMElement $img */
+            $imgsrc = $img->getAttribute('src');
 
-            // Ignore image sources that don't contain "pluginfile.php". We probably don't need
-            // to handle those, and would probably don't know ow to handle them anyway.
-            if (false === strpos($img->getAttribute('src'), 'pluginfile.php')) {
+            // Ignore data urls.
+            if (0 === strpos($imgsrc, 'data:')) {
                 continue;
             }
 
-            $imgsrc = $img->getAttribute('src');
-            $parts = explode('pluginfile.php/', $imgsrc);
-            $parts = explode('?', $parts[1]); // Remove any query params.
-            $filepathargs = $parts[0];
+            $dataurl = '';
 
-            $fileargs = explode('/', $filepathargs);
-            $contextid = array_shift($fileargs);
-            $component = array_shift($fileargs);
-            $filearea = array_shift($fileargs);
+            // Check if the IMG elements SRC attribute contains a local file that likely requires
+            // authentication by checking if the URL starts with our WWWROOT and contains a path
+            // element that matches "pluginfile.php".
+            $urlparts = explode('/', $imgsrc);
+            if (0 === strpos($imgsrc, $CFG->wwwroot) && in_array('pluginfile.php', $urlparts)) {
 
-            $imgfilename = urldecode(array_pop($fileargs));
-            $itemid = array_pop($fileargs);
+                // Seems to be a local file likely requiring authentication, i.e. we need to get the file
+                // content from the moodle file system by using the relevant arguments contained
+                // within the URL.
+
+                $parts = explode('pluginfile.php/', $imgsrc);
+                $parts = explode('?', $parts[1]); // Remove any query params.
+                $filepathargs = $parts[0];
+
+                $fileargs = explode('/', $filepathargs);
+                $contextid = array_shift($fileargs);
+                $component = array_shift($fileargs);
+                $filearea = array_shift($fileargs);
+
+                $imgfilename = urldecode(array_pop($fileargs));
+                $itemid = array_pop($fileargs);
 
 
-            $filerec = $DB->get_record('files', ['component' => $component, 'filearea' => $filearea, 'itemid' => $itemid, 'filename' => $imgfilename, 'contextid' => $contextid]);
+                $filerec = $DB->get_record('files', ['component' => $component, 'filearea' => $filearea, 'itemid' => $itemid, 'filename' => $imgfilename, 'contextid' => $contextid]);
 
-            if (!empty($filerec)) {
-                $fs = get_file_storage();
-                $file = $fs->get_file_instance($filerec);
-                $imgcontent = $file->get_content();
-                $dataurl = 'data:'.$file->get_mimetype().';base64,' . base64_encode($imgcontent);
+                if (!empty($filerec)) {
+                    $fs = get_file_storage();
+                    $file = $fs->get_file_instance($filerec);
+                    $imgcontent = $file->get_content();
+                    $dataurl = 'data:' . $file->get_mimetype() . ';base64,' . base64_encode($imgcontent);
+                }
+
             } else {
+
+                // Not a local file that requires authentication. Simply load the image content
+                // from the URL if possible and build a data url from it.
+
+                $imgcontent = file_get_contents($imgsrc);
+                if (!empty($imgcontent)) {
+
+                    // Get the filename extension.
+                    $parts = explode('.', $imgsrc);
+                    $filetype = array_pop($parts);
+
+                    // Pretend that the filename extension "svg" actually indicates an SVG file
+                    // and handle the file content accordingly.
+                    if ($filetype == 'svg') {
+
+                        $dataurl = 'data:image/svg+xml;base64,' . base64_encode($imgcontent);
+
+                    } else {
+
+                        // Let GD decide if it wants to handle the file content. If it does, we will
+                        // save it as PNG and embed that one. IF GD does not like the content, we will
+                        // let the image default to our placeholder.
+                        if ($imgres = imagecreatefromstring($imgcontent)) {
+
+                            // Get PNG version of the file content.
+                            ob_start();
+                            imagepng($imgres);
+                            $imagecontent = ob_get_contents();
+                            ob_end_clean();
+                            imagedestroy($imgres);
+
+                            // Create data url.
+                            $dataurl = 'data:image/png;base64,' . base64_encode($imagecontent);
+                        }
+                    }
+                }
+            }
+
+            // Fallback to placeholder image.
+            if (empty($dataurl)) {
                 $imgcontent = file_get_contents($CFG->dirroot . '/local/quizattemptexport/pix/edit-delete.png');
                 $dataurl = 'data:image/png;base64,' . base64_encode($imgcontent);
             }
